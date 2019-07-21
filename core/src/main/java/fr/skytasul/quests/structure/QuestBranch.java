275f3d3cd33f3d3f3a4b4d9f1a655444a0f98729
SkytAsul <skytasul@gmail.com>
2019-07-21 17:23:28 +0200

    Setstage method bugfix
    * seems like resources management wants to turn me crazy
    * fixing a bug when player is offline and a stage is set for its account

--- core/src/main/java/fr/skytasul/quests/structure/QuestBranch.java
+++ core/src/main/java/fr/skytasul/quests/structure/QuestBranch.java
@@ -200,11 +200,11 @@ public class QuestBranch {
 			}else manager.playerAdvancement.put(acc, new PlayerAdvancement(this));
 			if (p != null && launchStage){
 				stage.launch(p);
+				Utils.playPluginSound(p.getLocation(), "ITEM_FIRECHARGE_USE", 0.5F);
+				if (QuestsConfiguration.showNextParticles()) QuestsConfiguration.getParticleNext().send(p, Arrays.asList(p));
 			}
 			stage.start(acc);
 			Bukkit.getPluginManager().callEvent(new PlayerSetStageEvent(acc, getQuest(), stage));
-			Utils.playPluginSound(p.getLocation(), "ITEM_FIRECHARGE_USE", 0.5F);
-			if (QuestsConfiguration.showNextParticles()) QuestsConfiguration.getParticleNext().send(p, Arrays.asList(p));
 		}
 	}
 	
