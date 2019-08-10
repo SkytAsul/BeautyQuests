311ffb17c83fed3dea965d0d805d2f9b4e09b6ab
SkytAsul <skytasul@gmail.com>
2019-08-10 16:02:48 +0200

    Locales update
    * updated locales
    * added missing language entries from config.yml and source files

--- core/src/main/java/fr/skytasul/quests/BeautyQuests.java
+++ core/src/main/java/fr/skytasul/quests/BeautyQuests.java
@@ -214,7 +214,7 @@ public class BeautyQuests extends JavaPlugin{
 	
 	private YamlConfiguration loadLang() {
 		try {
-			for (String language : new String[]{"en_US", "fr_FR", "zh_CN", "de_DE", "pt_PT", "it_IT"}){
+			for (String language : new String[]{"en_US", "fr_FR", "zh_CN", "de_DE", "pt_PT", "it_IT", "es_ES"}){
 				File file = new File(getDataFolder(), "locales/" + language + ".yml");
 				if (!file.exists()) saveResource("locales/" + language + ".yml", false);
 			}
