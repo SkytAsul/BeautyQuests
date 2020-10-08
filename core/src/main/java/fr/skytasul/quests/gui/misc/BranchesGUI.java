b145d721054bba176e8b679f0fe5d1101f5ae434
SkytAsul <skytasul@gmail.com>
2020-10-08 19:21:30 +0200

    Up

--- core/src/main/java/fr/skytasul/quests/gui/misc/BranchesGUI.java
+++ core/src/main/java/fr/skytasul/quests/gui/misc/BranchesGUI.java
@@ -1,9 +1,7 @@
 package fr.skytasul.quests.gui.misc;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.util.function.Consumer;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -17,28 +15,47 @@ import fr.skytasul.quests.utils.XMaterial;
 
 public class BranchesGUI implements CustomInventory {
 	
-	private Branch main = new Branch();
+	private Branch main = new Branch(null);
 	
 	private Inventory inv;
-	private Branch shown;
 	
-	private int xStart = 0;
+	private Branch shown;
+	private int xOffset = 0;
 	
 	@Override
 	public Inventory open(Player p) {
 		inv = Bukkit.createInventory(null, 54, "Branches");
 		
-		inv.setItem(52, ItemUtils.itemLaterPage);
-		inv.setItem(53, ItemUtils.itemNextPage);
+		inv.setItem(49, ItemUtils.item(XMaterial.DIAMOND_BLOCK, "§bBack one branch"));
+		
+		inv.setItem(52, ItemUtils.item(XMaterial.ARROW, "§aScroll left"));
+		inv.setItem(53, ItemUtils.item(XMaterial.ARROW, "§aScroll right"));
 		
 		// TODO voir fichier image schéma
 		
-		displayBranch(main);
+		main.things.add(new Thing());
+		main.things.add(new Thing());
+		main.things.add(new Thing());
+		Branch branch = new Branch(main);
+		branch.things.add(new Thing());
+		branch.things.add(new Thing());
+		branch.things.add(new Thing());
+		main.choices.add(branch);
+		branch = new Branch(main);
+		branch.things.add(new Thing());
+		branch.things.add(new Thing());
+		main.choices.add(branch);
+		
+		setBranch(main);
 		
 		return p.openInventory(inv).getTopInventory();
 	}
 	
-	private void displayBranch(Branch branch) {
+	public void refresh() {
+		setBranch(shown);
+	}
+	
+	private void setBranch(Branch branch) {
 		this.shown = branch;
 		for (int i = 0; i < 45; i++) {
 			inv.clear(i);
@@ -46,59 +63,130 @@ public class BranchesGUI implements CustomInventory {
 		
 		int y = 2;
 		int start = y * 9;
-		//int to = Math.min(xStart + 9, shown.choices.size());
-		boolean create = true;
+		displayBranch(shown, start, xOffset, true);
+	}
+	
+	private void displayBranch(Branch branch, int start, int xOffset, boolean showBranches) {
 		int to;
-		if (shown.choices.size() >= xStart + 9) {
-			to = xStart + 9;
-			create = false;
+		if (branch.things.size() >= xOffset + 9) {
+			to = xOffset + 9;
+			showBranches = false; // no space to continue
 		}else {
-			to = shown.choices.size();
+			to = branch.things.size();
 		}
-		for (int i = xStart; i < to; i++) {
-			Thing thing = shown.things.get(i);
-			inv.setItem(start + i, thing.getItem(i == to - 1 ? shown.choices.size() == 0 ? XMaterial.RED_STAINED_GLASS_PANE : XMaterial.ORANGE_STAINED_GLASS_PANE : XMaterial.LIGHT_BLUE_STAINED_GLASS_PANE));
+		for (int i = xOffset; i < to; i++) {
+			IThing thing = branch.things.get(i);
+			inv.setItem(start + i - xOffset, thing.getItem(i == to - 1 ? branch.choices.isEmpty() ? XMaterial.RED_STAINED_GLASS_PANE : XMaterial.ORANGE_STAINED_GLASS_PANE : XMaterial.LIGHT_BLUE_STAINED_GLASS_PANE));
 		}
-		if (!create) return;
-		if (shown.choices.size() == 0) {
-			inv.setItem(start + to, ItemUtils.item(XMaterial.SLIME_BALL, "§eCreate next thing", "§8> LEFT CLICK : §7Create normal thing", "§8> RIGHT CLICK : §7Create choices"));
+		if (!showBranches) return;
+		if (branch.choices.isEmpty()) { // no branch at the end
+			inv.setItem(start + to - xOffset, ItemUtils.item(XMaterial.SLIME_BALL, "§eCreate next thing", "§8> LEFT CLICK : §7Create normal thing", "§8> RIGHT CLICK : §7Create choices"));
 		}else {
 			int i = 0;
-			for (Entry<Thing, Branch> entry : shown.choices.entrySet()) {
-				inv.setItem(i * 9 + to, entry.getKey().getItem(XMaterial.LIGHT_BLUE_STAINED_GLASS_PANE));
+			for (Branch endBranch : branch.choices) {
+				displayBranch(endBranch, i * 9 + to - xOffset, 0, false);
 				i++;
 			}
 			for (; i < 5; i++) {
-				inv.setItem(i * 9 + to, ItemUtils.item(XMaterial.SLIME_BALL, "§6Create choice"));
+				inv.setItem(i * 9 + to - xOffset, ItemUtils.item(XMaterial.SLIME_BALL, "§6Create choice"));
 			}
 		}
+	}
+	
+	public void create(Consumer<IThing> thing) {
+		thing.accept(new Thing());
 	}
 	
 	@Override
 	public boolean onClick(Player p, Inventory inv, ItemStack current, int slot, ClickType click) {
 		if (slot == 52) {
-			if (xStart > 0) {
-				xStart--;
-				displayBranch(shown);
+			if (xOffset > 0) {
+				xOffset--;
+				refresh();
 			}
 		}else if (slot == 53) {
-			xStart++;
-			displayBranch(shown);
+			if (xOffset < main.things.size()) {
+				xOffset++;
+				refresh();
+			}
+		}else if (slot == 49) {
+			if (shown.parent != null) setBranch(shown.parent);
+		}else {
+			int y = (int) (slot / 9D);
+			int x = slot - (y * 9);
+			int xThing = xOffset + x;
+			if (xThing < shown.things.size()) {
+				p.sendMessage("You clicked on thing " + shown.things.get(xThing).getID());
+			}else {
+				if (shown.choices.isEmpty()) {
+					p.sendMessage("You want to create thing at place " + xThing);
+					if (click.isLeftClick()) {
+						create(thing -> {
+							main.things.add(thing);
+							refresh();
+						});
+					}else if (click.isRightClick()) {
+						p.sendMessage("You want to create branch at place " + xThing);
+						create(thing -> {
+							Branch branch = new Branch(shown);
+							branch.things.add(thing);
+							main.choices.add(branch);
+							refresh();
+						});
+					}
+				}else if (y < shown.choices.size()) {
+					Branch branch = shown.choices.get(y);
+					xThing -= shown.things.size();
+					p.sendMessage("You clicked on thing " + branch.things.get(xThing).getID());
+					if (branch != shown) setBranch(shown);
+				}else {
+					p.sendMessage("You want to create a branch at place " + y);
+					create(thing -> {
+						Branch branch = new Branch(shown);
+						branch.things.add(thing);
+						main.choices.add(branch);
+						refresh();
+					});
+				}
+			}
 		}
 		return true;
 	}
 	
-	static class Branch {
-		LinkedList<Thing> things = new LinkedList<>();
-		Map<Thing, Branch> choices = new HashMap<>();
+	@Override
+	public CloseBehavior onClose(Player p, Inventory inv) {
+		return CloseBehavior.REMOVE;
 	}
 	
-	static class Thing {
+	static class Branch {
+		LinkedList<IThing> things = new LinkedList<>();
+		LinkedList<Branch> choices = new LinkedList<>();
+		
+		Branch parent;
+		
+		Branch(Branch parent) {
+			this.parent = parent;
+		}
+	}
+	
+	static interface IThing {
+		int getID();
+		
+		ItemStack getItem(XMaterial type);
+	}
+	
+	static class Thing implements IThing {
 		private static int counter = 0;
 		
 		private int id = counter++;
 		
-		ItemStack getItem(XMaterial type) {
+		@Override
+		public int getID() {
+			return id;
+		}
+		
+		@Override
+		public ItemStack getItem(XMaterial type) {
 			return ItemUtils.item(type, "§4Thing §b§l" + id);
 		}
 	}
