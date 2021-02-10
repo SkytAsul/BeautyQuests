e61ba706141269765587924b507eccbbd4665b2f
ChProgrammer <51345261+ChProgrammer@users.noreply.github.com>
2021-02-10 10:41:38 -0300

    Update Lang.java (#155)

--- core/src/main/java/fr/skytasul/quests/utils/Lang.java
+++ core/src/main/java/fr/skytasul/quests/utils/Lang.java
@@ -203,6 +203,7 @@ public enum Lang{
 	DIALOG_MSG_ADDED_PLAYER("msg.editor.dialog.player"),
 	DIALOG_MSG_ADDED_NPC("msg.editor.dialog.npc"),
 	DIALOG_MSG_ADDED_NOSENDER("msg.editor.dialog.noSender"),
+	DIALOG_MSG_EDITED("msg.editor.dialog.edited"),
 	DIALOG_MSG_REMOVED("msg.editor.dialog.messageRemoved"),
 	DIALOG_SOUND_ADDED("msg.editor.dialog.soundAdded"),
 	DIALOG_TIME_SET("msg.editor.dialog.timeSet"), // 0: index, 1: time
@@ -217,6 +218,7 @@ public enum Lang{
 	DIALOG_HELP_NPCINSERT("msg.editor.dialog.help.npcInsert"),
 	DIALOG_HELP_PLAYERINSERT("msg.editor.dialog.help.playerInsert"),
 	DIALOG_HELP_NOTHINGINSERT("msg.editor.dialog.help.nothingInsert"),
+	DIALOG_HELP_EDIT("msg.editor.dialog.help.edit"),
 	DIALOG_HELP_ADDSOUND("msg.editor.dialog.help.addSound"),
 	DIALOG_HELP_SETTIME("msg.editor.dialog.help.setTime"),
 	DIALOG_HELP_CLEAR("msg.editor.dialog.help.clear"),
