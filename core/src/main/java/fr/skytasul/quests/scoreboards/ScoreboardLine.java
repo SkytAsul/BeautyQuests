82143efddab63645474680ede29e91821d8c5362
SkytAsul <skytasul@gmail.com>
2023-03-02 10:21:39 +0100

    :bug: Fixed hex colors not working in scoreboards

--- core/src/main/java/fr/skytasul/quests/scoreboards/ScoreboardLine.java
+++ core/src/main/java/fr/skytasul/quests/scoreboards/ScoreboardLine.java
@@ -1,42 +1,45 @@
 package fr.skytasul.quests.scoreboards;
 
 import java.util.Map;
-
 import org.apache.commons.lang.Validate;
+import fr.skytasul.quests.utils.ChatUtils;
+import net.md_5.bungee.api.ChatColor;
 
 public class ScoreboardLine {
 
 	private String value;
 	private int refresh = 0;
 	private int length = 0;
-	
-	public ScoreboardLine(String value){
+
+	public ScoreboardLine(String value) {
 		Validate.notNull(value);
-		this.value = value
-				.replace("&", "§")
-				.replace("{questName}", "{quest_name}")
-				.replace("{questDescription}", "{quest_advancement}");
+		this.value = ChatColor.translateAlternateColorCodes('&',
+				ChatUtils.translateHexColorCodes(value
+						.replace("{questName}", "{quest_name}")
+						.replace("{questDescription}", "{quest_advancement}")));
 	}
-	
-	public String getValue(){
+
+	public String getValue() {
 		return value;
 	}
-	
-	public int getRefreshTime(){
+
+	public int getRefreshTime() {
 		return refresh;
 	}
-	
-	public int getMaxLength(){
+
+	public int getMaxLength() {
 		return length;
 	}
-	
-	public static ScoreboardLine deserialize(Map<String, Object> map){
+
+	public static ScoreboardLine deserialize(Map<String, Object> map) {
 		ScoreboardLine line = new ScoreboardLine((String) map.get("value"));
-		
-		if (map.containsKey("refresh")) line.refresh = (int) map.get("refresh");
-		if (map.containsKey("length")) line.length = (int) map.get("length");
-		
+
+		if (map.containsKey("refresh"))
+			line.refresh = (int) map.get("refresh");
+		if (map.containsKey("length"))
+			line.length = (int) map.get("length");
+
 		return line;
 	}
-	
+
 }
