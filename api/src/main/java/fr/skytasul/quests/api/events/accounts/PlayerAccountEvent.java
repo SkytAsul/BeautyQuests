fe9c2609466c9bdba76ff843c4aca203d615795b
SkytAsul <skytasul@gmail.com>
2024-05-24 19:33:14 +0200

    :goal_net: Catch bizarre things in account loading

--- api/src/main/java/fr/skytasul/quests/api/events/accounts/PlayerAccountEvent.java
+++ api/src/main/java/fr/skytasul/quests/api/events/accounts/PlayerAccountEvent.java
@@ -1,26 +1,26 @@
 package fr.skytasul.quests.api.events.accounts;
 
+import fr.skytasul.quests.api.players.PlayerAccount;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import fr.skytasul.quests.api.players.PlayerAccount;
 
 public abstract class PlayerAccountEvent extends Event {
-	
+
 	protected final @NotNull PlayerAccount account;
 
 	protected PlayerAccountEvent(@NotNull PlayerAccount account) {
 		this.account = account;
 	}
-	
+
 	public boolean isAccountCurrent() {
 		return account.isCurrent();
 	}
-	
+
 	public @Nullable Player getPlayer() {
 		if (!account.isCurrent())
-			throw new IllegalStateException("Account is not currently used");
+			throw new IllegalStateException("Account " + account.debugName() + " is not currently used");
 
 		return account.getPlayer();
 	}
@@ -28,5 +28,5 @@ public abstract class PlayerAccountEvent extends Event {
 	public @NotNull PlayerAccount getPlayerAccount() {
 		return account;
 	}
-	
+
 }
