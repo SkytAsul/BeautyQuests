636873f62fc85ad10e06dad67b91dd7dec24a23a
SkytAsul <skytasul@gmail.com>
2024-08-25 16:15:42 +0200

    🐛 Fixed visual bug in timer with huge values

--- core/src/main/java/fr/skytasul/quests/options/OptionTimer.java
+++ core/src/main/java/fr/skytasul/quests/options/OptionTimer.java
@@ -1,7 +1,5 @@
 package fr.skytasul.quests.options;
 
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.inventory.ItemStack;
 import fr.skytasul.quests.api.editors.TextEditor;
 import fr.skytasul.quests.api.editors.parsers.DurationParser.MinecraftTimeUnit;
 import fr.skytasul.quests.api.gui.ItemUtils;
@@ -11,42 +9,45 @@ import fr.skytasul.quests.api.options.QuestOption;
 import fr.skytasul.quests.api.quests.creation.QuestCreationGuiClickEvent;
 import fr.skytasul.quests.api.utils.Utils;
 import fr.skytasul.quests.api.utils.XMaterial;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.inventory.ItemStack;
 
 public class OptionTimer extends QuestOption<Integer> {
-	
+
 	public OptionTimer() {
 		super(OptionRepeatable.class);
 	}
-	
+
 	@Override
 	public Object save() {
 		return getValue();
 	}
-	
+
 	@Override
 	public void load(ConfigurationSection config, String key) {
 		setValue(config.getInt(key));
 	}
-	
+
 	@Override
 	public Integer cloneValue(Integer value) {
 		return value;
 	}
-	
+
 	@Override
 	public boolean shouldDisplay(OptionSet options) {
 		return options.getOption(OptionRepeatable.class).getValue();
 	}
-	
+
 	@Override
 	public ItemStack getItemStack(OptionSet options) {
 		return ItemUtils.item(XMaterial.CLOCK, Lang.timer.toString(), getLore());
 	}
-	
+
 	private String[] getLore() {
-		return new String[] { formatDescription(Lang.timerLore.toString()), "", formatValue(Utils.millisToHumanString(getValue() * 60 * 1000)) };
+		return new String[] {formatDescription(Lang.timerLore.toString()), "",
+				formatValue(Utils.millisToHumanString(getValue() * 60L * 1000L))};
 	}
-	
+
 	@Override
 	public void click(QuestCreationGuiClickEvent event) {
 		Lang.TIMER.send(event.getPlayer());
@@ -59,5 +60,5 @@ public class OptionTimer extends QuestOption<Integer> {
 			event.reopen();
 		}, MinecraftTimeUnit.MINUTE.getParser()).passNullIntoEndConsumer().start();
 	}
-	
+
 }
