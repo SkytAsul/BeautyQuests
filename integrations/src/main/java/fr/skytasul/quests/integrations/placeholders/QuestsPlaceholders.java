c55a01aaf89d44d65c3a868054d83021267bf8ec
SkytAsul <skytasul@gmail.com>
2025-12-29 21:49:03 +0100

    ✨🐛 Fixed build error + added pool_ID_can_start_reason placeholder

--- integrations/src/main/java/fr/skytasul/quests/integrations/placeholders/QuestsPlaceholders.java
+++ integrations/src/main/java/fr/skytasul/quests/integrations/placeholders/QuestsPlaceholders.java
@@ -32,7 +32,7 @@ public class QuestsPlaceholders extends PlaceholderExpansion implements Listener
 	private static QuestsPlaceholders placeholders;
 
 	private static final Pattern POOl_PLACEHOLDER_PATTERN =
-			Pattern.compile("pool_(\\d+)_(can_start|remaining|in_progress|completed|cooldown)");
+			Pattern.compile("pool_(\\d+)_(can_start|can_start_reason|remaining|in_progress|completed|cooldown)");
 
 	private final int lineLength;
 	private final int changeTime;
@@ -94,7 +94,8 @@ public class QuestsPlaceholders extends PlaceholderExpansion implements Listener
 		return Arrays.asList("total_amount", "quest_restartable_ID", "player_inprogress_amount", "player_finished_amount",
 				"player_finished_total_amount", "started", "started_ordered", "started_ordered_X", "advancement_ID",
 				"advancement_ID_raw", "player_quest_finished_ID", "started_id_list",
-				"pool_ID_can_start", "pool_ID_remaining", "pool_ID_in_progress", "pool_ID_completed", "pool_ID_cooldown");
+				"pool_ID_can_start", "pool_ID_can_start_reason", "pool_ID_remaining", "pool_ID_in_progress",
+				"pool_ID_completed", "pool_ID_cooldown");
 	}
 
 	@Override
@@ -254,7 +255,8 @@ public class QuestsPlaceholders extends PlaceholderExpansion implements Listener
 				return "error: unknown pool %d".formatted(poolId);
 
 			return switch (poolMatcher.group(2)) {
-				case "can_start" -> pool.canGive(p).result() ? "true" : "false";
+				case "can_start" -> pool.canGive(p).isSuccess() ? "true" : "false";
+				case "can_start_reason" -> pool.canGive(p).type().name();
 				case "remaining" -> Integer.toString(pool.getQuestsRemaining(quester).size());
 				case "in_progress" -> Integer.toString(pool.getQuestsInProgress(quester).size());
 				case "completed" -> Integer.toString(pool.getQuestsCompleted(quester).size());
