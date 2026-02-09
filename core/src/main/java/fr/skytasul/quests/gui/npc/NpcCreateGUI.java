ba64a5153cc1ace4052a7980c5e5989fa6e7c8ed
SkytAsul <skytasul@gmail.com>
2026-02-09 23:02:50 +0100

    🐛 Fixed NPC creation GUI

--- core/src/main/java/fr/skytasul/quests/gui/npc/NpcCreateGUI.java
+++ core/src/main/java/fr/skytasul/quests/gui/npc/NpcCreateGUI.java
@@ -4,137 +4,108 @@ import com.cryptomorin.xseries.XMaterial;
 import fr.skytasul.quests.api.QuestsPlugin;
 import fr.skytasul.quests.api.editors.TextEditor;
 import fr.skytasul.quests.api.editors.WaitClick;
-import fr.skytasul.quests.api.gui.AbstractGui;
-import fr.skytasul.quests.api.gui.GuiClickEvent;
 import fr.skytasul.quests.api.gui.ItemUtils;
-import fr.skytasul.quests.api.gui.close.CloseBehavior;
 import fr.skytasul.quests.api.gui.close.DelayCloseBehavior;
+import fr.skytasul.quests.api.gui.layout.LayoutedButton;
+import fr.skytasul.quests.api.gui.layout.LayoutedClickEvent;
+import fr.skytasul.quests.api.gui.layout.LayoutedGUI.LayoutedRowsGUI;
 import fr.skytasul.quests.api.localization.Lang;
 import fr.skytasul.quests.api.npcs.BqInternalNpcFactory.BqInternalNpcFactoryCreatable;
 import fr.skytasul.quests.api.npcs.BqNpc;
 import fr.skytasul.quests.api.options.QuestOption;
 import fr.skytasul.quests.api.utils.Utils;
 import fr.skytasul.quests.api.utils.messaging.DefaultErrors;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import java.util.HashMap;
 import java.util.function.Consumer;
 
-public class NpcCreateGUI extends AbstractGui {
+public class NpcCreateGUI extends LayoutedRowsGUI {
 
-	private static final ItemStack nameItem = ItemUtils.item(XMaterial.NAME_TAG, Lang.name.toString());
-	private static final ItemStack move = ItemUtils.item(XMaterial.MINECART, Lang.move.toString(), Lang.moveLore.toString());
 	public static ItemStack validMove = ItemUtils.item(XMaterial.EMERALD, Lang.moveItem.toString());
 
 	private final @NotNull BqInternalNpcFactoryCreatable factory;
 	private final @NotNull Consumer<@NotNull BqNpc> end;
 	private final @NotNull Runnable cancel;
 
-	private EntityType en;
-	private String name;
-	private String skin;
+	private EntityType en = EntityType.PLAYER;
+	private String name = "§cUnknown";
+	private String skin = "Knight";
 
 	public NpcCreateGUI(@NotNull BqInternalNpcFactoryCreatable factory, @NotNull Consumer<@NotNull BqNpc> end,
 			@NotNull Runnable cancel) {
+		super(Lang.INVENTORY_NPC.toString(), new HashMap<>(), new DelayCloseBehavior(cancel), 1);
+
 		this.factory = factory;
 		this.end = end;
 		this.cancel = cancel;
+
+		buttons.put(0, LayoutedButton.create(ItemUtils.item(XMaterial.MINECART, Lang.move.toString(),
+				QuestOption.formatDescription(Lang.moveLore.toString())), this::onMoveClick));
+		buttons.put(1,
+				LayoutedButton.createLoreValue(XMaterial.NAME_TAG, Lang.name.toString(), () -> name, this::onNameClick));
+		buttons.put(3,
+				LayoutedButton.create(
+						() -> ItemUtils.skull(Lang.skin.toString(), skin, QuestOption.formatNullableValue(skin)),
+						this::onSkinClick));
+		buttons.put(5, LayoutedButton.create(() -> {
+			if (en == EntityType.PLAYER) {
+				return ItemUtils.skull(Lang.npcType.toString(), null, QuestOption.formatNullableValue("player"));
+			} else
+				return ItemUtils.item(Utils.mobItem(en), Lang.npcType.toString(),
+						QuestOption.formatNullableValue(en.getName()));
+		}, this::onEntityTypeClick));
+
+		buttons.put(7, LayoutedButton.create(QuestsPlugin.getPlugin().getGuiManager().getItemFactory().getCancel(),
+				this::onCancelClick));
+		buttons.put(8, LayoutedButton.create(QuestsPlugin.getPlugin().getGuiManager().getItemFactory().getDone(),
+				this::onDoneClick));
 	}
 
-	@Override
-	protected Inventory instanciate(@NotNull Player player) {
-		return Bukkit.createInventory(null, 9, Lang.INVENTORY_NPC.toString());
+	private void onMoveClick(LayoutedClickEvent event) {
+		new WaitClick(event.getPlayer(), event::reopen, validMove.clone(), event::reopen).start();
 	}
 
-	@Override
-	protected void populate(@NotNull Player player, @NotNull Inventory inventory) {
-		inventory.setItem(0, move.clone());
-		inventory.setItem(1, nameItem.clone());
-		setName("§cno name selected");
-		setSkin("Knight");
-		setType(EntityType.PLAYER);
-		inventory.setItem(7, QuestsPlugin.getPlugin().getGuiManager().getItemFactory().getCancel());
-		inventory.setItem(8, QuestsPlugin.getPlugin().getGuiManager().getItemFactory().getDone());
+	private void onNameClick(LayoutedClickEvent event) {
+		Lang.NPC_NAME.send(event.getPlayer());
+		new TextEditor<String>(event.getPlayer(), event::reopen, obj -> {
+			this.name = obj;
+			event.refreshItemReopen();
+		}).start();
 	}
 
-	private void setName(String name) {
-		this.name = name;
-		ItemUtils.lore(getInventory().getItem(1), QuestOption.formatNullableValue(name));
+	private void onSkinClick(LayoutedClickEvent event) {
+		Lang.NPC_SKIN.send(event.getPlayer());
+		new TextEditor<String>(event.getPlayer(), event::reopen, obj -> {
+			this.skin = obj;
+			event.refreshItemReopen();
+		}).useStrippedMessage().start();
 	}
 
-	private void setType(EntityType type) {
-		this.en = type;
-		if (en == EntityType.PLAYER) {
-			getInventory().setItem(5, ItemUtils.skull(Lang.npcType.toString(), null, QuestOption.formatNullableValue("player")));
-		} else
-			getInventory().setItem(5,
-					ItemUtils.item(Utils.mobItem(en), Lang.npcType.toString(),
-							QuestOption.formatNullableValue(en.getName())));
+	private void onEntityTypeClick(LayoutedClickEvent event) {
+		QuestsPlugin.getPlugin().getGuiManager().getFactory().createEntityTypeSelection(en -> {
+			this.en = en;
+			event.refreshItemReopen();
+		}, x -> x != null && factory.isValidEntityType(x)).open(event.getPlayer());
 	}
 
-	private void setSkin(String skin) {
-		this.skin = skin;
-		getInventory().setItem(3, ItemUtils.skull(Lang.skin.toString(), skin, QuestOption.formatNullableValue(skin)));
+	private void onCancelClick(LayoutedClickEvent event) {
+		event.close();
+		cancel.run();
 	}
 
-	@Override
-	public void onClick(GuiClickEvent event) {
-		switch (event.getSlot()) {
-
-		case 0:
-				new WaitClick(event.getPlayer(), event::reopen, validMove.clone(), event::reopen).start();
-			break;
-
-		case 1:
-			Lang.NPC_NAME.send(event.getPlayer());
-			new TextEditor<String>(event.getPlayer(), event::reopen, obj -> {
-				setName(obj);
-				event.reopen();
-			}).start();
-			break;
-
-		case 3:
-			Lang.NPC_SKIN.send(event.getPlayer());
-			new TextEditor<String>(event.getPlayer(), event::reopen, obj -> {
-				if (obj != null) setSkin(obj);
-				event.reopen();
-			}).useStrippedMessage().start();
-			break;
-
-		case 5:
-			QuestsPlugin.getPlugin().getGuiManager().getFactory().createEntityTypeSelection(en -> {
-				setType(en);
-				event.reopen();
-			}, x -> x != null && factory.isValidEntityType(x)).open(event.getPlayer());
-			break;
-
-		case 7:
-			event.close();
+	private void onDoneClick(LayoutedClickEvent event) {
+		event.close();
+		try {
+			end.accept(QuestsPlugin.getPlugin().getNpcManager().createNPC(factory, event.getPlayer().getLocation(), en,
+					name, skin));
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			DefaultErrors.sendGeneric(QuestsPlugin.getPlugin().getAudiences().player(event.getPlayer()),
+					"npc creation " + ex.getMessage());
 			cancel.run();
-			break;
-
-		case 8:
-			event.close();
-			try {
-				end.accept(QuestsPlugin.getPlugin().getNpcManager().createNPC(factory, event.getPlayer().getLocation(), en,
-						name, skin));
-			}catch (Exception ex) {
-				ex.printStackTrace();
-				DefaultErrors.sendGeneric(QuestsPlugin.getPlugin().getAudiences().player(event.getPlayer()),
-						"npc creation " + ex.getMessage());
-				cancel.run();
-			}
-			break;
-
 		}
-	}
-
-	@Override
-	public CloseBehavior onClose(Player p) {
-		return new DelayCloseBehavior(cancel);
 	}
 
 }
